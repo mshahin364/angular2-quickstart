e3d62a25e5c9b8b9b4c2e3a594e883aea6f75458
mshahin364 <mshahin364@gmail.com>
2016-03-21 12:30:49 +0600

    Modify some text

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: [
+        '<h1>My First Angular 2 App</h1>',
+        '<h2>Get Started...</h2>'
+    ].join('')
 })
 export class AppComponent { }
