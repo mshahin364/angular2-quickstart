b981b072c284aea15c62e64bebe30c956b69792e
mshahin364 <mshahin364@gmail.com>
2016-03-21 16:37:19 +0600

    Hello world component added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,15 @@
 import {Component} from 'angular2/core';
+import {HelloWorldComponent} from './app.helloworld';
+
 
 @Component({
     selector: 'my-app',
     template: [
         '<h1>My First Angular 2 App</h1>',
-        '<h2>Get Started...</h2>'
-    ].join('')
+        '<h2>Get Started...</h2>',
+        '<hello-world>Hello....</hello-world>'
+    ].join(''),
+directives: [HelloWorldComponent]
 })
-export class AppComponent { }
+export class AppComponent { };
+
---
+++ app/app.helloworld.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'hello-world',
+    template:'<h1>Hello world</h1>'    
+})
+
+export class HelloWorldComponent{};
